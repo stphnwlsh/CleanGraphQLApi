e30dcc9546ba0f4eb45368ee183245ad51ad539f
Stephen Walsh <868017+stphnwlsh@users.noreply.github.com>
2021-11-14 17:35:36 +1000

    Fix dependencies and DI (#4)
    * Fix Dependencies
    
    * Clean up Presentation DI

---
+++ src/Presentation/DependencyInjection.cs
@@ -0,0 +1,23 @@
+namespace CleanGraphQL.Presentation;
+
+using System.Diagnostics.CodeAnalysis;
+using CleanGraphQL.Presentation.MovieReviews;
+using GraphQL;
+using GraphQL.SystemTextJson;
+using Microsoft.Extensions.DependencyInjection;
+
+[ExcludeFromCodeCoverage]
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        _ = services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        _ = services.AddSingleton<IDocumentWriter, DocumentWriter>();
+
+        _ = services.AddSingleton<MovieReviewQueries>();
+        _ = services.AddSingleton<MovieReviewMutations>();
+        _ = services.AddSingleton<MovieReviewSchema>();
+
+        return services;
+    }
+}
--- src/Presentation/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Presentation/Extensions/WebApplicationBuilderExtensions.cs
@@ -7,7 +7,6 @@ using CleanGraphQL.Infrastructure;
 using CleanGraphQL.Presentation.MovieReviews;
 using GraphQL;
 using GraphQL.Server;
-using GraphQL.SystemTextJson;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -18,7 +17,7 @@ public static class WebApplicationBuilderExtensions
     public static WebApplicationBuilder ConfigureBuilder(this WebApplicationBuilder builder)
     {
         #region Logging
-        _ = builder.Services.AddLogging(options => options.AddConsole());
+
         _ = builder.Host.UseSerilog((hostContext, loggerConfiguration) =>
         {
             var assembly = Assembly.GetEntryAssembly();
@@ -49,7 +48,7 @@ public static class WebApplicationBuilderExtensions
             .AddErrorInfoProvider(opt => opt.ExposeExceptionStackTrace = true)
             //.AddWebSockets() // Add required services for web socket support
             .AddDataLoader() // Add required services for DataLoader support
-            .AddGraphTypes(typeof(MovieReviewSchema)); // Add all IGraphType implementors in assembly which ChatSchema exists 
+            .AddGraphTypes(typeof(MovieReviewSchema)); // Add all IGraphType implementors in assembly which ChatSchema exists
 
         #endregion GraphQL
 
@@ -57,13 +56,7 @@ public static class WebApplicationBuilderExtensions
 
         _ = builder.Services.AddInfrastructure();
         _ = builder.Services.AddApplication();
-
-        _ = builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        _ = builder.Services.AddSingleton<IDocumentWriter, DocumentWriter>();
-
-        _ = builder.Services.AddSingleton<MovieReviewQueries>();
-        _ = builder.Services.AddSingleton<MovieReviewMutations>();
-        _ = builder.Services.AddSingleton<MovieReviewSchema>();
+        _ = builder.Services.AddPresentation();
 
         #endregion Project Dependencies
 
